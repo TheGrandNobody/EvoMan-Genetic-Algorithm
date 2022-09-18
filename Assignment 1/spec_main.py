df8c5123ace562757a1646e96e93a63d3c63d6b4
TheGrandNobody <thegrandnobody@protonmail.com>
2022-09-18 13:02:31 +0200

    Add NEAT config file

--- Assignment 1/spec_main.py
+++ Assignment 1/spec_main.py
@@ -4,7 +4,7 @@ from environment import Environment
 from controllers import specialist
 import numpy as np
 
-experiment_name = 'specialist_controller'
+experiment_name = 'A1_specialist'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
@@ -12,25 +12,11 @@ if not os.path.exists(experiment_name):
 env = Environment(experiment_name=experiment_name,
 				  playermode="ai",
 				  player_controller=specialist(),
-			  	  speed="fastest",
+			  	speed="fastest",
 				  enemymode="static",
 				  level=2)
 
-# saves results
-file_aux  = open(experiment_name+'/results.txt','a')
-print( '\n GENERATION '+str(i)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6)))
-file_aux.write('\n'+str(i)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6))   )
-file_aux.close()
-
-# saves generation number
-file_aux  = open(experiment_name+'/gen.txt','w')
-file_aux.write(str(i))
-file_aux.close()
-
-# saves file with the best solution
-np.savetxt(experiment_name+'/best.txt',pop[best])
-
-# saves simulation state
-solutions = [pop, fit_pop]
-env.update_solutions(solutions)
-env.save_state()
+# 
+headless = True
+if headless:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
