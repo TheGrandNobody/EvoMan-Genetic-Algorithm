403d5bb9585f098f8243850c1ac8c7ffb456f6b6
TheGrandNobody <thegrandnobody@protonmail.com>
2022-09-26 14:56:27 +0200

    Add a temporary neat pickle

--- substrate.py
+++ substrate.py
@@ -1,14 +1,16 @@
 """
-The substrate.
+The substrate that is optimized by the CPPN.
 """
 
 class Substrate(object):
     """
     Represents a substrate: Input coordinates, output coordinates, hidden coordinates and a resolution defaulting to 10.0.
+    Input and output coordinates are automatically calculated based on the number of inputs and outputs.
+    There is one hidden layer by default.
     """
 
-    def __init__(self, input_coordinates, output_coordinates, hidden_coordinates=(), res=10.0):
-        self.input_coordinates = input_coordinates
-        self.hidden_coordinates = hidden_coordinates
-        self.output_coordinates = output_coordinates
+    def __init__(self, num_inputs, num_outputs, hidden_coordinates=(), res=10.0):
+        self.input_coordinates = [(float(i), -1.0) for i in range(-20//2, 20//2)]
+        self.output_coordinates = [(float(i), 1.0) for i in range(-2, 3)]
+        self.hidden_coordinates = [[(float(i), 0.0) for i in range(-10//2, 10//2)]]
         self.res = res
