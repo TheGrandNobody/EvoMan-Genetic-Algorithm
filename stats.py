8efed24596b7fb96e8cdd9d6ec21a2a545cf95d2
Frederikevanesch <frederike1999@gmail.com>
2022-10-01 14:07:55 +0200

    yes

--- stats.py
+++ stats.py
@@ -2,21 +2,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-#Import the statistiek files 
-BubbleManStatsFile = pd.read_csv('BubbleManStatsFile.csv')
-BubbleManStats = BubbleManStatsFile.str.split(',', expand=True)
 
-result_BubbleMan = []
-
-for i in range (2,75,3):
-    mean_genaration = BubbleManStats[i].mean()
-    max_genaration = BubbleManStats[i+1].max()
-    result_BubbleMan.append(mean_genaration)
+#Start making the line plot over the different genarations 
+#plt.style.use('_mpl-gallery')
+name = ["BubbleManneatStatsFile.csv", "BubbleManesneatStatsFile.csv", "CrashManneatStatsFile.csv", "CrashManesneatStatsFile.csv", "WoodManneatStatsFile.csv", "WoodManesneatStatsFile.csv"]
 
 
-for i in range(3,75,3):
-    max_genaration = BubbleManStats[i].max()
-    result_BubbleMan.append(max_genaration)
+BubbleManneat_Stats = pd.read_csv("BubbleManneatStatsFile.csv")
+BubbleManneat_Stats = BubbleManneat_Stats.str.split(',', expand=True)
+
+BubbleManneat_Stats_results = []
+BubbleManneat_Stats_results = [range(1,15)]
+
+
+for j in range (2,46,3):
+    mean_mean_genaration = BubbleManneat_Stats[j].mean()
+    stdev_mean_genaration = BubbleManneat_Stats[j].stdev()
+    mean_max_genaration = BubbleManneat_Stats[j+1].max()
+    stdev_max_genaration = BubbleManneat_Stats[j+1].stdev()
+    BubbleManneat_Stats_results.append(mean_mean_genaration)
+    BubbleManneat_Stats_results.append(stdev_mean_genaration)
+    BubbleManneat_Stats_results.append(stdev_max_genaration)
+    BubbleManneat_Stats_results.append(mean_max_genaration)
+
+#Making line plot and adding mean and standaard deviation 
+    line_plot_results = plt.plot (BubbleManneat_Stats_results[2])
+    line_plot_results = plt.fill_between(range(len(BubbleManneat_Stats_results)),BubbleManneat_Stats_results[2]-BubbleManneat_Stats_results[3],BubbleManneat_Stats_results[2]+BubbleManneat_Stats_results[3],alpha=.1)
+    line_plot_results = plt.plot (BubbleManneat_Stats_results[5])
+    line_plot_results = plt.fill_between(range(len(BubbleManneat_Stats_results)),BubbleManneat_Stats_results[5]-BubbleManneat_Stats_results[4],BubbleManneat_Stats_results[5]+BubbleManneat_Stats_results[4],alpha=.1)
+
+#Making boxplot of the best individuals that were tested 
 
 
 
