789aed4aa13db20f7d2987678253ea892f30dbc2
kuhusinha9 <kuhusinha92@gmail.com>
2022-10-05 12:49:42 +0200

    removed warnings

--- test_generalist.py
+++ test_generalist.py
@@ -4,8 +4,6 @@ from environment import Environment
 from controllers import specialist
 import pickle
 import neat
-from es_hyperneat import ESNetwork
-from substrate import Substrate
 
 # Whether we are training using HyperNeat or not
 HYPERNEAT = len(sys.argv) > 1
@@ -15,7 +13,7 @@ env = Environment(experiment_name='logs',
               playermode="ai",
               multiplemode="yes",
               enemies=[1,2,3,4,5,6,7,8],
-              player_controller=specialist() if NEAT else simple(),
+              player_controller=specialist(),
               speed="fastest",
               enemymode="static",
               level=2)
@@ -34,13 +32,7 @@ if __name__ == "__main__":
             unpickler = pickle.Unpickler(f)
             genome = unpickler.load()
         # Create either an RNN or a CPPN
-        if HYPERNEAT:
-            sub = Substrate(20, 5)
-            cppn = neat.nn.FeedForwardNetwork.create(genome, config)
-            network = ESNetwork(sub, cppn)
-            rnn = network.create_phenotype_network()
-        else:
-            rnn = neat.nn.RecurrentNetwork.create(genome, config)
+        rnn = neat.nn.RecurrentNetwork.create(genome, config)
     
         #statsfile.write() for formatting later
     
