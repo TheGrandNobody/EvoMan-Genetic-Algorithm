8267ff1b7bde92bb32056ac3244226932ebf75da
TheGrandNobody <thegrandnobody@protonmail.com>
2022-10-10 14:01:25 +0200

    Update

--- run_muli_test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-for i in range(0,6):
-    subprocess.Popen('python enemy_test.py ' + str(i))
-
--- test.py
+++ test.py
@@ -11,6 +11,7 @@ from extra.substrate import Substrate
 
 # Whether we are training using HyperNeat or not
 NEAT = len(sys.argv) == 1
+NAME = '2,6,'
 ENEMIES_GENERAL = "1 2 3 4 5 6 7 8"
 
 # Initialize an environment for a specialist game (single objective) with a static enemy and an ai-controlled player
@@ -25,30 +26,30 @@ env = Environment(experiment_name='logs',
 
 # Open boxplot stats file 
 statsfile = open(r"test.csv", "a")
-statsfile.write("test_17,"+ "neat," if NEAT else "esneat,")
+statsfile.write("test_"+ NAME+ "neat," if NEAT else "esneat,")
 
 if __name__ == "__main__":
 
-    for i in range(0,3):
+    for i in range(0,10):
         statsfile.write(str(i) + ',')
         # Initialize the NEAT config 
         config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, 'configs/' + ('neat-generalist.cfg' if NEAT else 'esneat-generalist.cfg'))
-        with open('winners/test_generalist_17'+str(i)+'neat-winner.pkl', "rb") as f:
+        with open('winners/'+NAME+str(i)+'neat-winner.pkl', "rb") as f:
             unpickler = pickle.Unpickler(f)
             genome = unpickler.load()
         # Create either an Feedforward Network or a CPPN
         if NEAT:
-            rnn = neat.nn.FeedForwardNetwork.create(genome, config)
+            nn = neat.nn.FeedForwardNetwork.create(genome, config)
         else:
             sub = Substrate(20, 5)
             cppn = neat.nn.FeedForwardNetwork.create(genome, config)
             network = ESNetwork(sub, cppn)
             nn = network.create_phenotype_network()
     
-        a = env.play(rnn)
+        a = env.play(nn)
         print(a)
         # Write fitness value to stats file
-        statsfile.write(str(a[2] - a[1]) + ",")
+        statsfile.write(str(a[1] - a[2]) + ",")
     
 statsfile.write('\n')
 statsfile.close()
